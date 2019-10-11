477cffbb35f8ab88e2f6dce5c2d453da4682e0f2
Valiantsin Horbach <valentin.com2000gorbach@gmail.com>
2019-10-11 13:41:20 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let output='';
+    let buf;
+    for(i=0;i<expr.length;i++)
+    {
+        buf = expr.substr(i,10);
+        if(buf==='**********')
+        {
+            output+=' ';
+        }
+        else
+        {
+            while(buf.includes('1') || buf.includes('0'))
+            {
+                buf = buf.replace('00','');
+                buf = buf.replace('10','.');
+                buf = buf.replace('11','-');
+            }
+            output+=MORSE_TABLE[buf];
+        }
+        i+=9;
+    }
+    return output;
 }
+
 
 module.exports = {
     decode
